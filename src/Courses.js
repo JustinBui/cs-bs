960b69d3a9f6ee740e126885d0549bcbf77b6816
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-03-28 04:05:40 -0700

    Update Courses.js

--- src/Courses.js
+++ src/Courses.js
@@ -6,70 +6,79 @@ export function LowerDivs() {
 
         <tr>
           <th class="course">Course Name</th>
+          <th class="resources">Resources</th>
         </tr>
         {/* Courses */}
         <tr>
-          <th>
-            <a href="cpsc120">CPSC 120: Intro to Programming</a>
-          </th>
-        </tr>
-        <tr>
-          <th>
-            <a href="../components/cpsc121">
-              CSPC 121: Object Oriented Programming
+          <th>CSPC 120: Intro to Programm</th>
+          <th class="resources">
+            <a href="https://www.youtube.com/watch?v=18c3MTX0PK0&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb">
+              [1]
             </a>
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CSPC 121: Object-Oriented Programming</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc131">CSPC 131: Data Structures</a>{" "}
+            <a href="https://www.youtube.com/watch?v=wN0x9eZLix4">[1]</a>{" "}
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CSPC 131: Data Structures</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc223c">CSPC 223C: C Programming</a>{" "}
-          </th>
-        </tr>
-        <tr>
-          <th>
-            {" "}
-            <a href="../components/cpsc223j">
-              CSPC 223J: Java Programming
+            <a href="https://www.youtube.com/playlist?list=PLqODvuetmZCT1sN3Vd0vwvHeJPa3vSrC9 ">
+              [1]
             </a>{" "}
+            <a href="https://www.youtube.com/watch?v=B31LgI4Y4DQ">[2]</a>{" "}
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CSPC 223C: </th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc223n">
-              CSPC 223N: Visual C# Programming
-            </a>{" "}
+            <a href="https://www.youtube.com/watch?v=KJgsSFOSQv0">[1]</a>
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CSPC 223J: Java Programming</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc223p">
-              CSPC 223P: Python Programming
-            </a>{" "}
-          </th>
-        </tr>
-        <tr>
-          <th>
-            {" "}
-            <a href="../components/cpsc223w">
-              CSPC 223W: Swift Programming
-            </a>{" "}
-          </th>
-        </tr>
-        <tr>
-          <th>
-            {" "}
-            <a href="../components/cpsc240">
-              CSPC 240: Computer Org. and Assembly Lang.
+            <a href="https://www.youtube.com/watch?v=-QLskCQbLco&list=PLLAZ4kZ9dFpPpdR_9IQBUDLjYalvdrGGb">
+              [1]
             </a>
+          </th>
+        </tr>
+        <tr>
+          <th>CSPC 223N: Visual C# Programming</th>
+          <th class="resources">
+            {" "}
+            <a href="https://www.youtube.com/watch?v=GhQdlIFylQ8">[1]</a>
+          </th>
+        </tr>
+        <tr>
+          <th>CSPC 223P: Python Programming</th>
+          <th class="resources">
+            {" "}
+            <a href="https://youtu.be/rfscVS0vtbw">[1]</a>
+          </th>
+        </tr>
+        <tr>
+          <th>CSPC 223W: Swift Programming</th>
+          <th class="resources">
+            {" "}
+            <a href="https://www.youtube.com/watch?v=comQ1-x2a1Q ">[1]</a>
+          </th>
+        </tr>
+        <tr>
+          <th>CSPC 240: Computer Org. and Assembly Lang.</th>
+          <th class="resources">
+            {" "}
+            <a href="https://www.youtube.com/watch?v=eobVpYqvMGE&list=PLnd7R4Mcw3rJCvAduQxyySvejtBIaPs0O">
+              [1]
+            </a>{" "}
+            <a href="https://github.com/BuiTheBoi/x86-Assembly-Programs">[2]</a>
           </th>
         </tr>
       </thead>
@@ -86,52 +95,58 @@ export function UpperDivs() {
 
         <tr>
           <th class="course">Course Name</th>
+          <th class="resources">Resources</th>
         </tr>
         {/* Courses */}
         <tr>
-          <th>
+          <th>CSPC 254: Software Development with open source systems</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc254">
-              CSPC 254: Software Development with open source systems
+            <a href="https://www.youtube.com/watch?v=wyZq-EazOmU">[1]</a>{" "}
+            <a href="https://www.youtube.com/watch?v=-YqdwQXXw9k">[2]</a>{" "}
+            <a href="https://youtu.be/l4WXHdB_-98">[3]</a>{" "}
+          </th>
+        </tr>
+        <tr>
+          <th>CSPC 315: Professional Ethics in Computing</th>
+          <th class="resources">
+            {" "}
+            <a href="https://www.youtube.com/watch?v=_uEvWNdvSwA&list=PLoQGjel9343UNp0JFL6LrVuvYMh-7rWdC">
+              [1]
             </a>{" "}
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CPSC 323: Compilers and Languages</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc315">
-              CSPC 315: Professional Ethics in Computing
+            <a href="https://www.youtube.com/watch?v=W9B98S2mGGE&list=PL6KMWPQP_DM97Hh0PYNgJord-sANFTI3i">
+              [1]
             </a>{" "}
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CSPC 332: File Structures and Database Systems</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc323">
-              CPSC 323: Compilers and Languages
+            <a href="https://www.youtube.com/watch?v=aoMOmSx5Zyw&list=PLWPirh4EWFpHMw9bRo47tn4aknBUvuXNU">
+              [1]
             </a>{" "}
           </th>
         </tr>
         <tr>
-          <th>
-            <a href="../components/cpsc332">
-              CSPC 332: File Structures and Database Systems
-            </a>{" "}
+          <th>CSPC 335: Algorithm Engineering</th>
+          <th class="resources">
+            {" "}
+            <a href="https://youtu.be/HtSuA80QTyo">[1]</a>
           </th>
         </tr>
         <tr>
-          <th>
+          <th>CSPC 351: Operating Systems Concepts</th>
+          <th class="resources">
             {" "}
-            <a href="../components/cpsc335">
-              CSPC 335: Algorithm Engineering
-            </a>{" "}
-          </th>
-        </tr>
-        <tr>
-          <th>
-            {" "}
-            <a href="../components/cpsc351">
-              CSPC 351: Operating Systems Concepts
+            <a href="https://www.youtube.com/watch?v=vBURTt97EkA&list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O">
+              [1]
             </a>{" "}
           </th>
         </tr>
